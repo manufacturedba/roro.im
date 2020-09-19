9af244a1db6c51d088b6fc5a6a561a32e93554de
Roberto Rodriguez <manufacturedba@gmail.com>
2020-09-18 17:47:41 -0700

    Is this HTTPS?

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,3 +1,22 @@
 import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
+import { reads } from "@ember/object/computed";
+import config from "roro-im/config/environment";
 
-export default Route.extend({});
+export default Route.extend({
+  fastboot: service(),
+
+  beforeModel() {
+    if (
+      this.get("fastboot.isFastBoot") &&
+      this.get("fastboot.request.protocol") === "http:" &&
+      config.environment === "production"
+    ) {
+      const host = this.get("fastboot.request.host");
+
+      // Force upgrade through redirect
+      this.get("fastboot.response.headers").set("location", `https://${host}`);
+      this.set("fastboot.response.statusCode", 302);
+    }
+  }
+});
