6f5aa44e02814a9d29516926d08b04f7d5535db1
Roberto Rodriguez <manufacturedba@gmail.com>
2020-10-31 15:51:51 -0700

    Add temperature route

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = EmberRouter.extend({
 Router.map(function() {
   this.route("home", { path: "/" });
   this.route("camera");
+  this.route('temperature');
 });
 
 export default Router;
