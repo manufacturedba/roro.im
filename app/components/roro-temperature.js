dfce4e8a50bf5aeb0b6f50265dd01df8655bfce6
Roberto Rodriguez <manufacturedba@gmail.com>
2020-10-31 17:44:59 -0700

    Client temperature

---
+++ app/components/roro-temperature.js
@@ -0,0 +1,40 @@
+import Component from "@ember/component";
+import { computed } from "@ember/object";
+
+// Thanks https://stackoverflow.com/users/830134/nix
+function formatAMPM(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  const ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  return `${hours}:${minutes}${ampm.toUpperCase()}`;
+}
+
+export default Component.extend({
+  timestamp: computed("data", function() {
+    const data = this.get("data");
+    if (data) {
+      const date = new Date(data.date);
+      return formatAMPM(date);
+    }
+  }),
+
+  temperature: computed("data", function() {
+    const data = this.get("data");
+    if (data) {
+      return (data.temp * 9) / 5 + 32;
+    }
+  }),
+
+  didInsertElement() {
+    this._super(...arguments);
+    const socket = io();
+
+    socket.on("temperature", packet => {
+      console.log("Receiving a new packet");
+      this.set("data", packet);
+    });
+  }
+});
