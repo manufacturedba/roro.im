baf28c50de70f3a78e0bb30629dc6f4b576bfdb9
Roberto Rodriguez <manufacturedba@gmail.com>
2021-02-06 15:05:46 -0800

    k

--- app/components/pasta-show.js
+++ app/components/pasta-show.js
@@ -6,17 +6,16 @@ export default Component.extend({
   clock: service(),
 
   timeTill: computed("clock.time", function() {
-    const showTime = 1612643747269 + 5000000;
-    // 1612642905322 + 3620000;
-    const timeTill = showTime - this.get("clock.time");
+    // const showTime = 1612643747269 + 5000000;
+    // // 1612642905322 + 3620000;
+    // const timeTill = showTime - this.get("clock.time");
 
-    return (timeTill / 1000).toFixed();
+    // return (timeTill / 1000).toFixed();
+    return false;
   }),
 
   isLive: computed("clock.time", function() {
     // Now + 1:30
-    const showTime = 1612643747269 + 5000000;
-    console.log('Sanity check: ' + new Date(showTime).toString());
-    return this.get("clock.time") >= showTime;
+    return false;
   })
 });
